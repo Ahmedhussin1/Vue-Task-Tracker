b983ce3deac67a95e0c437be46a4262a8b1736ab
Ahmed Hussin <77442242+Ahmedhussin1@users.noreply.github.com>
2023-08-03 02:33:26 +0300

    Delete js/components directory

--- js/components/App.js
+++
@@ -1,9 +0,0 @@
-import AppButton from "./AppButton.js";
-import Assignments from "./Assignments.js";
-export default {
-    components:{Assignments},
-  template: `
-        <Assignments></Assignments>
-    `,
-  
-};
