2199238341af0786b10355f7c4b02b3008b66480
YE <ye@pop-os.localdomain>
2023-08-03 02:26:02 +0300

    task list made, add task button made but not functional yet

---
+++ traversy crash course/vue-crash/vue-app/src/components/Header.vue
@@ -0,0 +1,28 @@
+
+<template>
+   <header>
+    <h1>{{title}}</h1>
+    <Button color="green" text="Add Task"></Button>
+   </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name: 'Header',
+        props: ['title'],
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content : space-between;
+        align-items : center;
+        margin-bottom : 20px
+    }
+</style>
